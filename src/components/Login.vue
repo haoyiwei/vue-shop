0ef1f536d8c64ed6416e92090795d7e8029d23d8
hero <hyw163@163.com>
2019-04-21 15:24:01 +0800

    初始化vue_shop项目

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="login-container">
+    <div id="login-big">
+      <div id="login-center">
+        <img src="../assets/logo.png" alt>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+#login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  overflow: hidden;
+  #login-big {
+    width: 450px;
+    height: 304px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    #login-center {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 8px;
+      box-shadow: 0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
